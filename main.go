693825b1418fa539c40536fbb6f38946a444bcbe
Francis Somtochukwu <somtofrancis5@gmail.com>
2024-08-15 19:55:52 +0100

    Create a new filesystem in the container

--- main.go
+++ main.go
@@ -27,12 +27,13 @@ func main() {
 
 func run() {
 	fmt.Printf("Running %v\n", os.Args[2:])
+
 	cmd := exec.Command("/proc/self/exe", append([]string{"child"}, os.Args[2:]...)...)
 	cmd.Stdin = os.Stdin
 	cmd.Stdout = os.Stdout
 	cmd.Stderr = os.Stderr
 	cmd.SysProcAttr = &syscall.SysProcAttr{
-		Cloneflags: syscall.CLONE_NEWUTS,
+		Cloneflags: syscall.CLONE_NEWUTS | syscall.CLONE_NEWPID | syscall.CLONE_NEWNS,
 	}
 	if err := cmd.Run(); err != nil {
 		fmt.Printf("Error running child: %v\n", err)
@@ -43,8 +44,19 @@ func child() {
 	fmt.Println("This is the child function")
 	fmt.Printf("Running %v\n", os.Args[2:])
 
+	// Set the hostname
 	syscall.Sethostname([]byte("container"))
 
+	// Create a new directory for pivoting
+	must(os.MkdirAll("/rootfs", 0700))
+
+	// Bind mount root to /rootfs
+	must(syscall.Mount("/", "/rootfs", "", syscall.MS_BIND, ""))
+	must(os.MkdirAll("/rootfs/oldroot", 0700))
+	must(syscall.PivotRoot("/rootfs", "/rootfs/oldroot"))
+	must(os.Chdir("/"))
+
+	// Run the provided command
 	cmd := exec.Command(os.Args[2], os.Args[3:]...)
 	cmd.Stdin = os.Stdin
 	cmd.Stdout = os.Stdout
@@ -52,5 +64,11 @@ func child() {
 
 	if err := cmd.Run(); err != nil {
 		fmt.Printf("Error running command: %v\n", err)
+	}
+}
+
+func must(err error) {
+	if err != nil {
+		panic(err)
 	}
 }
